c46008f93a0816f665437fb7127d1c2aa05491e3
Bshara Zahran <bshara23@gmail.com>
2021-02-15 19:56:52 +0200

    ddd

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ app.use (express.json ());
 if (process.env.NODE_ENV === 'production') {
   app.use (express.static (path.join (__dirname, 'cs-frontend', 'build')));
 
-  app.get ('/', function (req, res) {
-    res.sendFile (path.join (__dirname, 'cs-frontend', 'build', 'index.html'));
-  });
+  // app.get ('/', function (req, res) {
+  //   res.sendFile (path.join (__dirname, 'cs-frontend', 'build', 'index.html'));
+  // });
 }
 
 const EMAIL = 'bshara23demo@gmail.com';
