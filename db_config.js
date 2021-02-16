b70c91746472f4e6b52deedfb090f5f2783b5247
Bshara Zahran <bshara23@gmail.com>
2021-02-16 21:12:16 +0200

    recapcha removed

--- db_config.js
+++ db_config.js
@@ -6,9 +6,13 @@ const local = {
   host: 'localhost',
   port: '5432',
   database: 'pcshop',
+  ssl: true,
 };
 const remote = {
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 const pool = new Pool (process.env.NODE_ENV === 'production' ? remote : local);
